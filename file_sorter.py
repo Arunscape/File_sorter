3b32c76d90d60eb52249435bd2624568d701b90a
Arunscape <8227297+Arunscape@users.noreply.github.com>
2018-01-15 20:45:56 -0700

    base functionality of moving files theoretically works on both windows and linux

--- file_sorter.py
+++ file_sorter.py
@@ -8,7 +8,16 @@ from getpass import getuser #to get username of the person running script
 #import setup
 #need to actually make setup file, for now just get base functionality working
 
-#def firstrun():
+#depending on the os, slashes for folders are different, and in the csv file,
+#windows locations are in the 2nd column, and linux locations are in the 3rd column
+if platform.startswith('linux'):
+    slash='/'
+    fldr_idx=2
+elif platform.startswith('win32'):
+    slash='\\'
+    fldr_idx=1
+else:
+    raise Exception('OS not supported, use Windows or Linux!')
 
 def get_dir():
     with open('settings.csv') as f:
@@ -22,6 +31,8 @@ def get_dir():
             while 1:
                 use_dwnld_fldr = input('Would you like to monitor and sort the downloads folder? (y)es/(n)o: ')
                 if use_dwnld_fldr.startswith('y'):
+                    #for some reason, had to make u a variable, otherwise when I tried to concatenate strings,
+                    #it would say function object at 0x>memory address< instead of the username
                     u=getuser()
                     if platform.startswith('linux'):
                         dir='/home/{}/Downloads'.format(u)
@@ -45,7 +56,8 @@ def get_dir():
         #update settings. Is costly, because it has to copy entire file to memory, then rewrite every single line
         #again but will stick with this so that it's still user friendly to change the setings for which file types
         #go where. If I was to switch to using some other data structure, then the user probably could not just edit
-        #the csv in excel, but would maybe have to edit JSON for example, or install pandas as a depencency, at least I think
+        #the csv in excel, but would maybe have to edit JSON for example, or install pandas as a depencency, at least
+        #I think. Also, it should only have to run once.
         with open('settings.csv') as f:
             data=list(csv.reader(f))
         data[0][1]=dir
@@ -53,4 +65,40 @@ def get_dir():
             csv.writer(f).writerows(data)
     return dir
 
-get_dir()
+def get_files_in_dir(dir):
+    #global slash
+    everything_in_folder=os.listdir(dir)
+    files_in_dir=[f for f in everything_in_folder if os.path.isfile(''.join([dir,slash,f]))]
+    return files_in_dir
+
+def sort_files(files_in_dir,dir):
+    #global slash, fldr_idx
+    with open('settings.csv') as f:
+        #skip the first 2 lines
+        next(csv.reader(f))
+        next(csv.reader(f))
+        #read relevant info from file
+        data=list(csv.reader(f))
+
+        for line in data:
+            for file in files_in_dir:
+               #if actual file extension == extension in csv file
+                if os.path.splitext(file)[1] == line[0]:
+                    #if: input file
+                    #of: output file
+                    _if=''.join([dir,slash,file])
+                    _of=''.join([line[fldr_idx],slash,file])
+
+                    try:
+                        #shutil.move(_if,_of)
+                        print('{}\nmoved to\n{}\n'.format(_if,_of))
+
+                    except FileNotFoundError:
+                        os.makedirs(line[fldr_idx])
+                        shutil.move(_if,_of)
+
+                    except:
+                        raise Exception('Something happened, file could not be moved.')
+
+dir=get_dir()
+sort_files(get_files_in_dir(dir),dir)
