da99f2c4e255043e1ae140793a18b2c42b71a2a4
Arunscape <arunscape@gmail.com>
2018-06-29 23:59:28 -0600

    ?

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+// import logo from "./logo.svg";
+// import "./App.css";
+
+// import { sideBar } from "./semantic/dist/components/sidebar";
+import { Header, Icon, Image, Menu, Segment, Sidebar } from "semantic-ui-react";
 
 // use electron stuff
 // const electron = window.require("electron");
@@ -12,16 +15,39 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <h1 className="App-title">Welcome to React</h1>
-      //   </header>
-      //   <p className="App-intro">
-      //     To get started, edit <code>src/App.js</code> and save to reload.
-      //   </p>
-      // </div>
-      <div>Hiiii</div>
+      <React.Fragment>
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar
+            as={Menu}
+            animation="overlay"
+            icon="labeled"
+            inverted
+            vertical
+            visible
+            width="thin"
+          >
+            <Menu.Item as="a">
+              <Icon name="home" />
+              Home
+            </Menu.Item>
+            <Menu.Item as="a">
+              <Icon name="gamepad" />
+              Games
+            </Menu.Item>
+            <Menu.Item as="a">
+              <Icon name="camera" />
+              Channels
+            </Menu.Item>
+          </Sidebar>
+
+          <Sidebar.Pusher>
+            <Segment basic>
+              <Header as="h3">Application Content</Header>
+              <Image src="/images/wireframe/paragraph.png" />
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </React.Fragment>
     );
   }
 }
