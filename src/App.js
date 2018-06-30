684dbcca410631404506024e496e286da85acdda
Arunscape <arunscape@gmail.com>
2018-06-29 23:42:43 -0600

    electron integration with react

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// use electron stuff
+// const electron = window.require("electron");
+//
+// const fs = electron.remote.require("fs");
+// const ipcRenderer = electron.ipcRenderer;
+// https://medium.freecodecamp.org/building-an-electron-application-with-create-react-app-97945861647c
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 className="App-title">Welcome to React</h1>
+      //   </header>
+      //   <p className="App-intro">
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      // </div>
+      <div>Hiiii</div>
     );
   }
 }
